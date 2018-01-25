a0a197b5a92b158060b6fd9e746e0eaf75f32a5d
teclandivar <teclandivar@laburl>
2018-01-25 07:43:40 -0600

    primer commit

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace Lab1_DianaGutierrez_23538
             Console.WriteLine("Escriba su nombre");
             string nombre = Console.ReadLine();
             Console.WriteLine("Hola " + nombre + " Este es su primer programa de Intro");
+            Console.WriteLine("HOLA MUNDO"); // cAMBIO 25/01
             Console.ReadLine();
         }
     }
